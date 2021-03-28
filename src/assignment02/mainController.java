3febd7d4958a4dc0525bf905d8f822d0e7da2c1d
Garrett Sisk <garrett@gsisk.com>
2021-03-28 02:11:49 -0400

    Added additional testing/debugging statements to puzzle.java. Have puzzle dynamics implemented and fully tested. Attempting to create simulated annealing algorithm for it now.

--- src/assignment02/mainController.java
+++ src/assignment02/mainController.java
@@ -1,6 +1,7 @@
 package assignment02;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -40,6 +41,17 @@ public class mainController {
     public TextField startState13;
     public TextField startState23;
     public TextField startState33;
+    public Button upButton;
+    public Button downButton;
+    public Button leftButton;
+    public Button rightButton;
+    public TextField upHeuristic;
+    public TextField downHeuristic;
+    public TextField leftHeuristic;
+    public TextField rightHeuristic;
+    public TextField startingTemp;
+
+    public puzzle workingPuzzle = new puzzle();
 
 
     // EVENT HANDLER METHODS
@@ -47,10 +59,39 @@ public class mainController {
     public void setRandomState() {
         // this method creates a puzzle object, and creates a random state, and displays the values in the appropriate
         // text fields
-        puzzle workingPuzzle = new puzzle();
         workingPuzzle.setRandomState();
+        drawGrid();
+        startingTemp.setText(Integer.toString(workingPuzzle.currentHeuristic()));
+    }
 
-        // write all values to the appropriate display block
+    public void moveZeroUp() {
+        workingPuzzle.moveUp();
+        drawGrid();
+        startingTemp.setText(Integer.toString(workingPuzzle.currentHeuristic()));
+    }
+
+    public void moveZeroDown() {
+        workingPuzzle.moveDown();
+        drawGrid();
+        startingTemp.setText(Integer.toString(workingPuzzle.currentHeuristic()));
+    }
+
+    public void moveZeroLeft() {
+        workingPuzzle.moveLeft();
+        drawGrid();
+        startingTemp.setText(Integer.toString(workingPuzzle.currentHeuristic()));
+    }
+
+    public void moveZeroRight() {
+        workingPuzzle.moveRight();
+        drawGrid();
+        startingTemp.setText(Integer.toString(workingPuzzle.currentHeuristic()));
+    }
+
+    // private methods
+
+    private void drawGrid() {
+        // this method redraws the current state grid to reflect the change in the puzzle state
         startState11.setText((Integer.toString(workingPuzzle.getCellValue(0,0))));
         startState21.setText((Integer.toString(workingPuzzle.getCellValue(1,0))));
         startState31.setText((Integer.toString(workingPuzzle.getCellValue(2,0))));
@@ -60,6 +101,13 @@ public class mainController {
         startState13.setText((Integer.toString(workingPuzzle.getCellValue(0,2))));
         startState23.setText((Integer.toString(workingPuzzle.getCellValue(1,2))));
         startState33.setText((Integer.toString(workingPuzzle.getCellValue(2,2))));
+
+        // recomputes the heuristics for each move as well
+        int[] nextValues = workingPuzzle.nextHeuristics();
+        upHeuristic.setText(Integer.toString(nextValues[0]));
+        downHeuristic.setText(Integer.toString(nextValues[1]));
+        leftHeuristic.setText(Integer.toString(nextValues[2]));
+        rightHeuristic.setText(Integer.toString(nextValues[3]));
     }
 
 
--- src/assignment02/puzzle.java
+++ src/assignment02/puzzle.java
@@ -1,6 +1,8 @@
 package assignment02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class puzzle {
@@ -15,21 +17,22 @@ public class puzzle {
     };
 
     // define solutionState as a 2D array of integers with zero denoting the blank space
-    private int[][] solutionState = {
+    private final int[][] solutionState = {
             {1, 2, 3},
             {8, 0, 4},
             {7, 6, 5}
     };
 
     // define solutionState as a collection of 2D coordinates for use in heuristic calculations
-    private int[] one = {0,0};
-    private int[] two = {1,0};
-    private int[] three = {2,0};
-    private int[] four = {2,1};
-    private int[] five = {2,2};
-    private int[] six = {1,2};
-    private int[] seven = {0,2};
-    private int[] eight = {0,1};
+    private final int[] zero = {1,1};
+    private final int[] one = {0,0};
+    private final int[] two = {1,0};
+    private final int[] three = {2,0};
+    private final int[] four = {2,1};
+    private final int[] five = {2,2};
+    private final int[] six = {1,2};
+    private final int[] seven = {0,2};
+    private final int[] eight = {0,1};
 
 
     // constructors
@@ -95,6 +98,51 @@ public class puzzle {
         this.puzzleState[y][x] = inputValue;
     }
 
+    public LinkedList<String> solvePuzzle(int startingTemp, int decrement, int quenchingTemp) {
+        // this method uses simulated annealing to solve the puzzle from a given state. It returns an array list of
+        // steps used to solve the problem
+
+        LinkedList<String> solutionStack = new LinkedList<>();
+
+        int currentValue, delta, minimumIndex;
+        int[] nextValue;
+
+        int currentTemp = startingTemp;
+
+        while (!(Arrays.deepEquals(puzzleState, solutionState))) {
+
+            currentValue = currentHeuristic();
+            nextValue = nextHeuristics();
+            minimumIndex = nextValue[4];
+            delta = currentValue - nextValue[minimumIndex];
+            if (delta < 0) {
+                if (minimumIndex == 0) {
+                    moveUp();
+                } else if (minimumIndex == 1) {
+                    moveDown();
+                } else if (minimumIndex == 2) {
+                    moveLeft();
+                } else if (minimumIndex == 3) {
+                    moveRight();
+                }
+            } else if ( delta >= 0) {
+                Random randomDouble = new Random();
+                double randValue = randomDouble.nextInt(100)/100.0;
+                double tempValue = Math.exp(-delta/(double)currentTemp);
+                if (randValue < tempValue) {
+                    // choose a random move to make
+                    Random rand = new Random();
+                    int randomMove = rand.nextInt(4);
+                    
+                }
+            }
+
+
+        }
+
+        return solutionStack;
+    }
+
     public String[] getPossibleMoves() {
         // this method creates a string array of possible moves = {'up','down','left','right'}
         String[] possibleMoves = {"","","",""};
@@ -103,25 +151,27 @@ public class puzzle {
         int[] blankLocation = findCellLocation(0);
 
         // determine if the "up" move is possible
-        if (blankLocation[1] == 2 || blankLocation[1] == 3) {
+        if (blankLocation[1] == 1 || blankLocation[1] == 2) {
             // location is on second or third row, thus an "up" move is possible
             possibleMoves[0] = "up";
         }
         // determine if "down" move is possible
-        if (blankLocation[1] == 1 || blankLocation[1] == 2) {
+        if (blankLocation[1] == 0 || blankLocation[1] == 1) {
             // location is on first and second row, thus a "down" move is possible
             possibleMoves[1] = "down";
         }
         // determine if the "left" move is possible
-        if (blankLocation[0] == 3 || blankLocation[0] == 2) {
+        if (blankLocation[0] == 2 || blankLocation[0] == 1) {
             // location is second or third column
             possibleMoves[2] = "left";
         }
         // determine if the "right" (direction) move is possible
-        if (blankLocation[0] == 1 || blankLocation[0] == 2) {
+        if (blankLocation[0] == 0 || blankLocation[0] == 1) {
             // location is on first or second column, thus a "right" (direction) is possible
             possibleMoves[3] = "right";
         }
+        System.out.println("Possible Moves are:\n");
+        System.out.println(possibleMoves[0] + ", " + possibleMoves[1] + ", " + possibleMoves[2] + ", " + possibleMoves[3] + "\n");
         return possibleMoves;
     }
 
@@ -131,49 +181,154 @@ public class puzzle {
         int[] outputCoordinates = {-1,-1};
         for (int yIndex = 0; yIndex < 3; yIndex++) {
             for (int xIndex = 0; xIndex < 3; xIndex++) {
-                if (puzzleState[xIndex][yIndex] == inputValue) {
+                if (puzzleState[yIndex][xIndex] == inputValue) {
                     outputCoordinates[0] = xIndex;
                     outputCoordinates[1] = yIndex;
-                    return outputCoordinates;
                 }
             }
         }
+        System.out.println("The cell with value " + inputValue + " is located at X = " + outputCoordinates[0] + ", " +
+                "and Y = " + outputCoordinates[1] + ".");
         return outputCoordinates;
     }
 
     public int currentHeuristic() {
         // returns the heuristic value for the current state. The heuristic used is the summation of moves for each
         // cell to move independently via a Manhattan Distance from its current location to the goal location
-        int totalValue = 0;
 
-        return totalValue;
+        // find the location of all elements
+        int[] zeroLocation = findCellLocation(0);
+        int[] oneLocation = findCellLocation(1);
+        int[] twoLocation = findCellLocation(2);
+        int[] threeLocation = findCellLocation(3);
+        int[] fourLocation = findCellLocation(4);
+        int[] fiveLocation = findCellLocation(5);
+        int[] sixLocation = findCellLocation(6);
+        int[] sevenLocation = findCellLocation(7);
+        int[] eightLocation = findCellLocation(8);
+
+        //Determine the distances (in moves) from current location to goal location
+        int zeroDistance = (Math.abs(zero[0] - zeroLocation[0]) + Math.abs(zero[1] - zeroLocation[1]));
+        int oneDistance = (Math.abs(one[0] - oneLocation[0]) + Math.abs(one[1] - oneLocation[1]));
+        int twoDistance = (Math.abs(two[0] - twoLocation[0]) + Math.abs(two[1] - twoLocation[1]));
+        int threeDistance = (Math.abs(three[0] - threeLocation[0]) + Math.abs(three[1] - threeLocation[1]));
+        int fourDistance = (Math.abs(four[0] - fourLocation[0]) + Math.abs(four[1] - fourLocation[1]));
+        int fiveDistance = (Math.abs(five[0] - fiveLocation[0]) + Math.abs(five[1] - fiveLocation[1]));
+        int sixDistance = (Math.abs(six[0] - sixLocation[0]) + Math.abs(six[1] - sixLocation[1]));
+        int sevenDistance = (Math.abs(seven[0] - sevenLocation[0]) + Math.abs(seven[1] - sevenLocation[1]));
+        int eightDistance = (Math.abs(eight[0] - eightLocation[0]) + Math.abs(eight[1] - eightLocation[1]));
+
+        // sum up the distances
+        return (zeroDistance + oneDistance + twoDistance + threeDistance + fourDistance + fiveDistance +
+                sixDistance + sevenDistance + eightDistance);
+
     }
 
     public int[] nextHeuristics() {
         // this method takes the current state, and performs all possible moves (up, down, left, right) and compiles an
         // array of values for each. If a move is not possible, the value is -1.
-        int[] heuristics = {-1,-1,-1,-1};
 
+        int[] heuristics = {-1, -1, -1, -1, -1};
+
+        String[] possibleMoves = getPossibleMoves();
+
+        if (possibleMoves[0].equalsIgnoreCase("up")) {
+            moveUp();
+            heuristics[0] = currentHeuristic();
+            moveDown();
+        }
+        if (possibleMoves[1].equalsIgnoreCase("down")) {
+            moveDown();
+            heuristics[1] = currentHeuristic();
+            moveUp();
+        }
+        if (possibleMoves[2].equalsIgnoreCase("left")) {
+            moveLeft();
+            heuristics[2] = currentHeuristic();
+            moveRight();
+        }
+        if (possibleMoves[3].equalsIgnoreCase("right")) {
+            moveRight();
+            heuristics[3] = currentHeuristic();
+            moveLeft();
+        }
+
+        int smallestValue = -1;
+        for (int i = 0; i < 4; i++) {
+            if (!(heuristics[i] == -1)) {
+                int currentValue = heuristics[i];
+                if (smallestValue == -1) {
+                    smallestValue = currentValue;
+                    heuristics[4] = i;
+                } else if (currentValue < smallestValue) {
+                    smallestValue = currentValue;
+                    heuristics[4] = i;
+                }
+            } else {
+                // do nothing because this element is -1, move to next one
+            }
+        }
+        System.out.println("The index of the smallest value is " + heuristics[4]);
         return heuristics;
     }
 
     public void moveUp() {
         // this method finds the blank square, and switches it with the square above it. It checks whether the cell can
         // actually be moved up. If it cannot, no action is performed and the console reports no move was made.
+        int[] blankLocation = findCellLocation(0);
+
+        if (blankLocation[1] == 0) {
+            // do nothing
+            System.out.println("The blank cell cannot perform 'moveUp' as it is on the top row.");
+        } else {
+            int swapValue = getCellValue(blankLocation[0], (blankLocation[1]-1));
+            setCellValue(0, blankLocation[0], (blankLocation[1]-1));
+            setCellValue(swapValue, blankLocation[0], blankLocation[1]);
+        }
     }
 
     public void moveDown() {
         // this method finds the blank square, and switches it with the square below it. It checks whether the cell can
         // actually be moved down. If it cannot, no action is performed and the console reports no move was made.
+        int[] blankLocation = findCellLocation(0);
+
+        if (blankLocation[1] == 2) {
+            // do nothing
+            System.out.println("The blank cell cannot perform 'moveDown' as it is on the bottom row.");
+        } else {
+            int swapValue = getCellValue(blankLocation[0], (blankLocation[1]+1));
+            setCellValue(0, blankLocation[0], (blankLocation[1]+1));
+            setCellValue(swapValue, blankLocation[0], blankLocation[1]);
+        }
     }
 
     public void moveLeft() {
         // this method finds the blank square, and switches it with the square to the left. It checks whether the cell
         // can actually be moved. If it cannot, no action is performed and the console reports no move was made.
+        int[] blankLocation = findCellLocation(0);
+
+        if (blankLocation[0] == 0) {
+            // do nothing
+            System.out.println("The blank cell cannot perform 'moveLeft' as it is on the left column.");
+        } else {
+            int swapValue = getCellValue((blankLocation[0]-1), (blankLocation[1]));
+            setCellValue(0, (blankLocation[0]-1), (blankLocation[1]));
+            setCellValue(swapValue, blankLocation[0], blankLocation[1]);
+        }
     }
 
     public void moveRight() {
         // this method finds the blank square, and switches it with the square to the right. It checks whether the cell
         // can actually be moved. If it cannot, no action is performed and the console reports no move was made.
+        int[] blankLocation = findCellLocation(0);
+
+        if (blankLocation[0] == 2) {
+            // do nothing
+            System.out.println("The blank cell cannot perform 'moveRight' as it is on the right column.");
+        } else {
+            int swapValue = getCellValue((blankLocation[0]+1), (blankLocation[1]));
+            setCellValue(0, (blankLocation[0]+1), (blankLocation[1]));
+            setCellValue(swapValue, blankLocation[0], blankLocation[1]);
+        }
     }
 }
